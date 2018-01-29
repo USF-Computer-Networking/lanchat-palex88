3201326e052a278bdff41f664a29904bbf844450
Alex Thompson <awt1088@gmail.com>
2018-01-28 18:23:09 -0800

    scapy working with p2.7 virtualenv

--- scan.py
+++ scan.py
@@ -1,13 +1,19 @@
+import sys
 import socket
+
+from scapy.all import *
 
 
 def get_host_name():
     return socket.gethostbyname(socket.gethostname())
 
 
-def check_lan():
-    return
+def check_lan(ip_address):
+    return scapy._version()
 
 
 if __name__ == '__main__':
-    print(get_host_name())
+
+    ip_address = get_host_name()
+    print ip_address
+    print check_lan(ip_address)

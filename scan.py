d710a01bd64f51ff7c32203bb715d575ee8465de
Alex Thompson <awt1088@gmail.com>
2018-01-29 17:38:42 -0800

    updates to scan, not subproccesses arp and creates ip address objects

--- scan.py
+++ scan.py
@@ -1,19 +1,33 @@
 import sys
 import socket
+from subprocess import check_output
 
 from scapy.all import *
+
+import scanipaddress
 
 
 def get_host_name():
     return socket.gethostbyname(socket.gethostname())
 
 
-def check_lan(ip_address):
-    return scapy._version()
+def arp_network():
+    return check_output(["arp", "-a"])
+
+
+def create_ip_addresses():
+    return
 
 
 if __name__ == '__main__':
 
-    ip_address = get_host_name()
-    print ip_address
-    print check_lan(ip_address)
+    local_ip_address = get_host_name()
+    test = (arp_network())
+
+    ip_objects = []
+    for arp_addr in test.splitlines():
+        ip_address = arp_addr.split(" ")[0]
+        host_name = arp_addr.split(" ")[1]
+        ip_objects.append(scanipaddress.ScanIpAddress(ip_address, host_name))
+
+    print len(ip_objects)

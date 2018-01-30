fa08efdda389ac6f55f62c235483d66fc68ac8d5
Alex Thompson <awt1088@gmail.com>
2018-01-29 20:33:52 -0800

    small updates to scan.py

--- scan.py
+++ scan.py
@@ -7,6 +7,9 @@ from scapy.all import *
 import scanipaddress
 
 
+def get_local_ip():
+    return get_host_name
+
 def get_host_name():
     return socket.gethostbyname(socket.gethostname())
 
@@ -15,19 +18,17 @@ def arp_network():
     return check_output(["arp", "-a"])
 
 
-def create_ip_addresses():
-    return
-
-
 if __name__ == '__main__':
 
-    local_ip_address = get_host_name()
-    test = (arp_network())
+    print "Your IP address is " + get_host_name() + "\n"
+
+    all_hosts = (arp_network())
 
     ip_objects = []
-    for arp_addr in test.splitlines():
+    for arp_addr in all_hosts.splitlines():
         ip_address = arp_addr.split(" ")[0]
         host_name = arp_addr.split(" ")[1]
         ip_objects.append(scanipaddress.ScanIpAddress(ip_address, host_name))
 
-    print len(ip_objects)
+    for host in ip_objects:
+        print host

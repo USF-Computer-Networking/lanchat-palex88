7fbe0f668310a7234a0c66936ebbd7f7dc44ed41
Alex Thompson <awt1088@gmail.com>
2018-02-26 17:36:02 -0800

    pep8 changes

--- chat_server.py
+++ chat_server.py
@@ -1,5 +1,6 @@
 import socket
 
+
 def run_server(UDP_IP_ADDRESS, UDP_PORT_NO):
     serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
@@ -7,6 +8,7 @@ def run_server(UDP_IP_ADDRESS, UDP_PORT_NO):
     while True:
         data, addr = serverSock.recvfrom(9999)
         print "Message: ", data
+
 
 if __name__ == '__main__':
     run_server("192.168.42.86", 9999)
